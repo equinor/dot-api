75a50fbab257e81943d87b18a09756a490182f1d
EinarSalbouvet <esalo@equinor.com>
2025-05-30 13:32:28 +0200

    refactor: add session handler to reduce repeated code

--- src/services/objective_service.py
+++ src/services/objective_service.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from src.models.objective import Objective
@@ -13,54 +12,40 @@ from src.dtos.user_dtos import (
 )
 from src.repositories.objective_repository import ObjectiveRepository
 from src.repositories.user_repository import UserRepository
+from src.services.session_handler import session_handler
 
 class ObjectiveService:
     def __init__(self, engine: AsyncEngine):
         self.engine=engine
 
     async def create(self, dtos: list[ObjectiveIncomingDto], user_dto: UserIncomingDto) -> list[ObjectiveOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
-                entities: list[Objective] = await ObjectiveRepository(session).create(ObjectiveMapper.to_entities(dtos, user.id))
-                # get the dtos while the entities are still connected to the session
-                result: list[ObjectiveOutgoingDto] = ObjectiveMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
+            entities: list[Objective] = await ObjectiveRepository(session).create(ObjectiveMapper.to_entities(dtos, user.id))
+            # get the dtos while the entities are still connected to the session
+            result: list[ObjectiveOutgoingDto] = ObjectiveMapper.to_outgoing_dtos(entities)
         return result
     
     async def update(self, dtos: list[ObjectiveIncomingDto], user_dto: UserIncomingDto) -> list[ObjectiveOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
-                entities: list[Objective] = await ObjectiveRepository(session).update(ObjectiveMapper.to_entities(dtos, user.id))
-                # get the dtos while the entities are still connected to the session
-                result: list[ObjectiveOutgoingDto] = ObjectiveMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
+            entities: list[Objective] = await ObjectiveRepository(session).update(ObjectiveMapper.to_entities(dtos, user.id))
+            # get the dtos while the entities are still connected to the session
+            result: list[ObjectiveOutgoingDto] = ObjectiveMapper.to_outgoing_dtos(entities)
         return result
     
     async def delete(self, ids: list[int]):
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                await ObjectiveRepository(session).delete(ids)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            await ObjectiveRepository(session).delete(ids)
     
     async def get(self, ids: list[int]) -> list[ObjectiveOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             objectives: list[Objective] = await ObjectiveRepository(session).get(ids)
             result=ObjectiveMapper.to_outgoing_dtos(objectives)
         return result
     
     async def get_all(self) -> list[ObjectiveOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             objectives: list[Objective] = await ObjectiveRepository(session).get_all()
             result=ObjectiveMapper.to_outgoing_dtos(objectives)
         return result
