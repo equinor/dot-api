0f0a46c54977204b361e659404b4b7b332be209f
EinarSalbouvet <129149934+EinarSalbouvet@users.noreply.github.com>
2025-06-02 12:15:25 +0200

    refactor: add session handler to reduce repeated code (#17)

--- src/services/scenario_service.py
+++ src/services/scenario_service.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from src.models.scenario import Scenario
@@ -11,64 +10,50 @@ from src.repositories.scenario_repository import ScenarioRepository
 from src.repositories.user_repository import UserRepository
 from src.repositories.objective_repository import ObjectiveRepository
 from src.repositories.opportunity_repository import OpportunityRepository
+from src.services.session_handler import session_handler
 
 class ScenarioService:
     def __init__(self, engine: AsyncEngine):
         self.engine=engine
 
     async def create(self, dtos: list[ScenarioCreateDto], user_dto: UserIncomingDto) -> list[ScenarioOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
-                # create scenario
-                entities: list[Scenario] = await ScenarioRepository(session).create(ScenarioMapper.from_create_to_entities(dtos, user.id))
+        async with session_handler(self.engine) as session:
+            user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
+            # create scenario
+            entities: list[Scenario] = await ScenarioRepository(session).create(ScenarioMapper.from_create_to_entities(dtos, user.id))
 
-                # create objectives/opportunities
-                for entity, dto in zip(entities, dtos):
-                    objectives=await ObjectiveRepository(session).create(ObjectiveMapper.via_scenario_to_entities(dto.objectives, user.id, entity.id))
-                    opportunities=await OpportunityRepository(session).create(OpportunityMapper.via_project_to_entities(dto.opportunities, user.id, entity.id))
+            # create objectives/opportunities
+            for entity, dto in zip(entities, dtos):
+                objectives=await ObjectiveRepository(session).create(ObjectiveMapper.via_scenario_to_entities(dto.objectives, user.id, entity.id))
+                opportunities=await OpportunityRepository(session).create(OpportunityMapper.via_project_to_entities(dto.opportunities, user.id, entity.id))
 
-                    entity.objectives=objectives
-                    entity.opportunities=opportunities
+                entity.objectives=objectives
+                entity.opportunities=opportunities
 
-                # get the dtos while the entities are still connected to the session
-                result: list[ScenarioOutgoingDto] = ScenarioMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+            # get the dtos while the entities are still connected to the session
+            result: list[ScenarioOutgoingDto] = ScenarioMapper.to_outgoing_dtos(entities)
         return result
     
     async def update(self, dtos: list[ScenarioIncomingDto], user_dto: UserIncomingDto) -> list[ScenarioOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
-                entities: list[Scenario] = await ScenarioRepository(session).update(ScenarioMapper.to_entities(dtos, user.id))
-                # get the dtos while the entities are still connected to the session
-                result: list[ScenarioOutgoingDto] = ScenarioMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            user=await UserRepository(session).get_or_create(UserMapper.to_entity(user_dto))
+            entities: list[Scenario] = await ScenarioRepository(session).update(ScenarioMapper.to_entities(dtos, user.id))
+            # get the dtos while the entities are still connected to the session
+            result: list[ScenarioOutgoingDto] = ScenarioMapper.to_outgoing_dtos(entities)
         return result
     
     async def delete(self, ids: list[int]):
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                await ScenarioRepository(session).delete(ids)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
-    
+        async with session_handler(self.engine) as session:
+            await ScenarioRepository(session).delete(ids)
+                
     async def get(self, ids: list[int]) -> list[ScenarioOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             scenarios: list[Scenario] = await ScenarioRepository(session).get(ids)
             result=ScenarioMapper.to_outgoing_dtos(scenarios)
         return result
     
     async def get_all(self) -> list[ScenarioOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             scenarios: list[Scenario] = await ScenarioRepository(session).get_all()
             result=ScenarioMapper.to_outgoing_dtos(scenarios)
         return result
--- src/services/uncertainty_service.py
+++ src/services/uncertainty_service.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from src.models.uncertainty import Uncertainty
@@ -8,52 +7,38 @@ from src.dtos.uncertainty_dtos import (
     UncertaintyMapper
 )
 from src.repositories.uncertainty_repository import UncertaintyRepository
+from src.services.session_handler import session_handler
 
 class UncertaintyService:
     def __init__(self, engine: AsyncEngine):
         self.engine=engine
 
     async def create(self, dtos: list[UncertaintyIncomingDto]) -> list[UncertaintyOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                entities: list[Uncertainty] = await UncertaintyRepository(session).create(UncertaintyMapper.to_entities(dtos))
-                # get the dtos while the entities are still connected to the session
-                result: list[UncertaintyOutgoingDto] = UncertaintyMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            entities: list[Uncertainty] = await UncertaintyRepository(session).create(UncertaintyMapper.to_entities(dtos))
+            # get the dtos while the entities are still connected to the session
+            result: list[UncertaintyOutgoingDto] = UncertaintyMapper.to_outgoing_dtos(entities)
         return result
     
     async def update(self, dtos: list[UncertaintyIncomingDto]) -> list[UncertaintyOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                entities: list[Uncertainty] = await UncertaintyRepository(session).update(UncertaintyMapper.to_entities(dtos))
-                # get the dtos while the entities are still connected to the session
-                result: list[UncertaintyOutgoingDto] = UncertaintyMapper.to_outgoing_dtos(entities)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            entities: list[Uncertainty] = await UncertaintyRepository(session).update(UncertaintyMapper.to_entities(dtos))
+            # get the dtos while the entities are still connected to the session
+            result: list[UncertaintyOutgoingDto] = UncertaintyMapper.to_outgoing_dtos(entities)
         return result
     
     async def delete(self, ids: list[int]):
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
-            try:
-                await UncertaintyRepository(session).delete(ids)
-                await session.commit()
-            except Exception as e:
-                await session.rollback()
-                raise e
+        async with session_handler(self.engine) as session:
+            await UncertaintyRepository(session).delete(ids)
     
     async def get(self, ids: list[int]) -> list[UncertaintyOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             decisions: list[Uncertainty] = await UncertaintyRepository(session).get(ids)
             result=UncertaintyMapper.to_outgoing_dtos(decisions)
         return result
     
     async def get_all(self) -> list[UncertaintyOutgoingDto]:
-        async with AsyncSession(self.engine, autoflush=True, autocommit=False) as session:
+        async with session_handler(self.engine) as session:
             decisions: list[Uncertainty] = await UncertaintyRepository(session).get_all()
             result=UncertaintyMapper.to_outgoing_dtos(decisions)
         return result
