34b3ed57a9b359ac5f4258612bed17b19e54d8fc
rupakkatwal <rupak2072@gmail.com>
2025-06-24 15:49:53 +0200

    fix

--- src/database.py
+++ src/database.py
@@ -3,4 +3,4 @@ from urllib import parse
 
 class connection_strings(Enum):
     sql_lite_memory="sqlite+aiosqlite:///:memory:"
-    ODBC_Msi_dev="mssql+aioodbc://decision-optimization-sqlserver-dev.database.windows.net:1433/decision-optimization-sqldb-dev?uid=decision-optimization-identity-dev&driver=ODBC+Driver+18+for+SQL+Server&authentication=ActiveDirectoryMsi&connectionTimeout=300"
+    ODBC_Msi_dev="mssql+aioodbc://tcp:decision-optimization-sqlserver-dev.database.windows.net:1433/decision-optimization-sqldb-dev?uid=decision-optimization-identity-dev&driver=ODBC+Driver+18+for+SQL+Server&authentication=ActiveDirectoryMsi&connectionTimeout=300"
