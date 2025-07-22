d36c06e39afc019bf02fd852512b6b6859f93c24
EinarSalbouvet <esalo@equinor.com>
2025-07-22 12:40:10 +0200

    refactor: remove temp user function

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -28,12 +28,6 @@ async def get_current_user(
             azure_id=upn
         )
         
-def get_temp_user() -> UserIncomingDto: 
-    """
-    Gets a usable user dto, should be replaced with a MS Graph service that gets the user information
-    """
-    return UserIncomingDto(id=None, azure_id="c35fd60c-60db-4fa6-891b-8edec06865f9", name="John Doe")
-
 class UserService:
     def __init__(self, engine: AsyncEngine):
         self.engine=engine
