2ee81e553ec1dd6e4469165f3d10e4fea8d74681
rupakkatwal <rupak2072@gmail.com>
2025-08-26 14:45:32 +0200

    remove project owner and contributor role

--- src/repositories/query_extensions.py
+++ src/repositories/query_extensions.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm.strategy_options import _AbstractLoad # type: ignore
 from sqlalchemy.orm import selectinload
-from sqlalchemy import select
 from src.models import (
     Issue,
     Node,
@@ -8,7 +7,6 @@ from src.models import (
     Scenario,
     Decision,
     Uncertainty,
-    User
 )
 
 class QueryExtensions:
@@ -76,8 +74,7 @@ class QueryExtensions:
             selectinload(Project.scenarios).options(
                 *QueryExtensions.load_scenario_with_relationships()
             ),
-            selectinload(Project.project_contributors),
-            selectinload(Project.project_owners)
+
         ]
 
     @staticmethod
@@ -86,12 +83,3 @@ class QueryExtensions:
         To be used as input for generic repositories when there are no relationships to be loaded.
         """
         return []
-    @staticmethod
-    def load_user_with_roles() -> list[_AbstractLoad]:
-        """
-        To be used as input for generic repositories to load user relationships.
-        """
-        return [
-            selectinload(User.project_contributors),
-            selectinload(User.project_owners)
-        ]
