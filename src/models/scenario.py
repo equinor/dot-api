7b33bee111576771aa2a527a5b0126eab77f736b
EinarSalbouvet <129149934+EinarSalbouvet@users.noreply.github.com>
2025-09-08 11:07:28 +0200

    Feat/default scenario (#95)
    * feat: add default scenarios
    
    * test: add tests for default scenarios
    
    * test: remove unessessary test portion

--- src/models/scenario.py
+++ src/models/scenario.py
@@ -1,11 +1,12 @@
 from typing import TYPE_CHECKING
 import uuid
 from src.models.guid import GUID
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Boolean
 from sqlalchemy.orm import (
     Mapped, 
     relationship, 
     mapped_column,
+    Session,
 )
 from src.models.base import Base
 from sqlalchemy.event import listens_for
@@ -29,6 +30,8 @@ class Scenario(Base, BaseEntity, BaseAuditableEntity):
     project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey(Project.id), index=True)
 
     name: Mapped[str] = mapped_column(String(DatabaseConstants.MAX_SHORT_STRING_LENGTH.value), index=True, default="")
+
+    is_default: Mapped[bool] = mapped_column(Boolean(), default=False)
 
     project: Mapped[Project] = relationship(Project, foreign_keys=[project_id])
 
@@ -58,17 +61,49 @@ class Scenario(Base, BaseEntity, BaseAuditableEntity):
         cascade="all, delete-orphan",
     )
 
-    def __init__(self, id: uuid.UUID, name: str, project_id: uuid.UUID, user_id: int, objectives: list["Objective"], opportunities: list["Opportunity"]):
+    def __init__(self, id: uuid.UUID, name: str, project_id: uuid.UUID, user_id: int, objectives: list["Objective"], opportunities: list["Opportunity"], is_default: bool=False):
         self.id = id
         self.name = name
         self.project_id = project_id
         self.updated_by_id = user_id
         self.objectives = objectives
         self.opportunities = opportunities
+        self.is_default=is_default
 
     def __repr__(self):
         return f"id: {self.id}, name: {self.name}"
 
+def default_scenario_rule(connection, target: Scenario):
+    """
+    Ensure that after 
+    """
+    session = Session(bind=connection)
+    
+    all_scenarios=session.query(Scenario).filter(
+        Scenario.project_id == target.project_id,
+    ).all()
+
+    if len(all_scenarios)==0:
+        return 
+    
+    if sum([x.is_default for x in all_scenarios]) != 1:
+        raise ValueError(
+                f"Project {target.project_id} must have exactly one default scenario."
+            )
+
 @listens_for(Scenario, "before_insert")
 def set_created_by_id(mapper, connection, target: Scenario): # type: ignore
     target.created_by_id = target.updated_by_id
+
+@listens_for(Scenario, "after_insert")
+def ensure_one_default_scenario_on_insert(mapper, connection, target: Scenario):  # type: ignore
+    default_scenario_rule(connection, target)
+
+
+@listens_for(Scenario, "after_update")
+def ensure_one_default_scenario_on_update(mapper, connection, target: Scenario):  # type: ignore
+    default_scenario_rule(connection, target)
+
+@listens_for(Scenario, "after_delete")
+def ensure_one_default_scenario_on_update(mapper, connection, target: Scenario):  # type: ignore
+    default_scenario_rule(connection, target)
