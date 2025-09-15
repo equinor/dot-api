9e500ccb5d464961d5c073a3773f42887c05d458
EinarSalbouvet <esalo@equinor.com>
2025-09-15 11:26:22 +0200

    fix: circular dependency

--- src/services/solver_service.py
+++ src/services/solver_service.py
@@ -1,7 +1,7 @@
 import uuid
 from src.services.pyagrum_solver import PyagrumSolver
 from src.services.scenario_service import ScenarioService
-from src.dependencies import get_db
+from src.session_manager import sessionmanager
 
 class SolverService:
     def __init__(
@@ -11,7 +11,7 @@ class SolverService:
         self.scenario_service=scenario_service
 
     async def find_optimal_decision_pyagrum(self, scenario_id: uuid.UUID):
-        async for session in get_db():
+        async for session in sessionmanager.get_session():
             issues, edges = await self.scenario_service.get_influence_diagram_data(session, scenario_id)
 
         solution = PyagrumSolver().find_optimal_decisions(issues=issues, edges=edges)
