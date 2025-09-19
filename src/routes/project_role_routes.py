361d33b354261316748733006e757624c1b94f36
EinarSalbouvet <esalo@equinor.com>
2025-09-19 07:35:26 +0200

    refactor: use session in role routes

--- src/routes/project_role_routes.py
+++ src/routes/project_role_routes.py
@@ -1,20 +1,23 @@
 from typing import Optional
 import uuid
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.dtos.project_roles_dtos import   ProjectRoleIncomingDto, ProjectRoleOutgoingDto
 from src.services.user_service import get_current_user
 from src.services.project_role_service import ProjectRoleService
 from src.dependencies import  get_project_role_service
 from src.dtos.user_dtos import UserIncomingDto
+from src.dependencies import get_db
 
 router = APIRouter(tags=["project-roles"])
     
 @router.get("/project-roles/")
 async def get_all_project_roles(
     project_role_service: ProjectRoleService = Depends(get_project_role_service),
+    session: AsyncSession = Depends(get_db),
 ) -> list[ProjectRoleOutgoingDto]:
     try:
-        project_roles: list[ProjectRoleOutgoingDto] = await project_role_service.get_all()
+        project_roles: list[ProjectRoleOutgoingDto] = await project_role_service.get_all(session)
         return project_roles
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -23,9 +26,10 @@ async def get_all_project_roles(
 async def get_project_role(
     id: uuid.UUID,
     project_role_service: ProjectRoleService = Depends(get_project_role_service),
+    session: AsyncSession = Depends(get_db),
 ) -> list[ProjectRoleOutgoingDto]:
     try:
-        project_roles: list[ProjectRoleOutgoingDto] = await project_role_service.get([id])
+        project_roles: list[ProjectRoleOutgoingDto] = await project_role_service.get(session, [id])
         return project_roles
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -35,11 +39,11 @@ async def delete_project_role(
     id: uuid.UUID,
     project_id: uuid.UUID,
     project_role_service: ProjectRoleService = Depends(get_project_role_service),
-    current_user: UserIncomingDto = Depends(get_current_user)
-
+    current_user: UserIncomingDto = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db),
 ):
     try:
-        await project_role_service.delete([id], project_id, current_user)
+        await project_role_service.delete(session, [id], project_id, current_user)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
@@ -47,9 +51,10 @@ async def delete_project_role(
 async def update_project_role(
     dto: list[ProjectRoleIncomingDto],
     project_role_service: ProjectRoleService = Depends(get_project_role_service),
-    current_user: UserIncomingDto = Depends(get_current_user)
+    current_user: UserIncomingDto = Depends(get_current_user),
+    session: AsyncSession = Depends(get_db),
 ) -> Optional[list[ProjectRoleOutgoingDto]]:
     try:
-        return await project_role_service.update( dto, current_user)
+        return await project_role_service.update(session, dto, current_user)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,13 @@ from src.dtos.user_dtos import UserIncomingDto
 from asgi_lifespan import LifespanManager
 from src.auth.auth import verify_token
 from src.services.user_service import get_current_user
-from src.main import app
+from src.main import app, config
+
+def check_test_environment():
+    if config.APP_ENV != "local":
+        raise Exception("Tests must be ran in local environment")
+    if ":memory:" not in config.DATABASE_CONN_LOCAL:
+        raise Exception("Tests must be ran using an in memory database")
 
 async def mock_verify_token():
     return
@@ -26,11 +32,13 @@ async def lifespan_manager() -> AsyncGenerator[None, None]:
     """
     Fixture to manage the app's lifespan (startup and shutdown) for the entire test session.
     """
+    check_test_environment()
     async with LifespanManager(app, startup_timeout=600, shutdown_timeout=600):
         yield 
 
 @pytest_asyncio.fixture
 async def client(lifespan_manager) -> AsyncGenerator[AsyncClient, None]:
+    check_test_environment()
     host, port = "127.0.0.1", 8080
     async with AsyncClient(transport=ASGITransport(app=app, client=(host, port)), base_url="http://test") as client:
         yield client
