54fdec276ad3ef61fa65fe806aa2bb47c54e0d87
Jarle Vidar Frette (Bouvet ASA) <JAFR@equinor.com>
2025-10-09 08:35:23 +0200

    Import cleanup

--- src/dtos/decision_tree_dtos.py
+++ src/dtos/decision_tree_dtos.py
@@ -1,19 +1,19 @@
+import uuid
 from typing import List, Optional
-from pydantic import BaseModel
-from typing import List
-from typing import Optional, List
 from pydantic import BaseModel, Field
 from src.dtos.issue_dtos import IssueOutgoingDto
-import uuid
+
 
 class EdgeUUIDDto(BaseModel):
     tail: uuid.UUID
     head: uuid.UUID | None
 
+
 class EndPointNodeDto(BaseModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
     scenario_id: uuid.UUID
     type: str = "EndPoint"
+
 
 class DecisionTreeDTO(BaseModel):
     id: IssueOutgoingDto | EndPointNodeDto
